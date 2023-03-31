4879f9c67ec646a37d819da0eabdf9a9e44f8d33
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-03-31 14:57:29 -0700

    adding basic route structure for mean, median and mode

--- math.js
+++ math.js
@@ -1,6 +1,47 @@
 const express = require("express");
-
 const app = express();
+
+// Route to calculate mean/ average
+app.get("/mean", (req, res) => {
+  // Throw error if empty query strings
+  if (!req.query.nums) throw Error("Missing Query Inputs", 400);
+
+  let numString = req.query.nums.split(",");
+  let result = {
+    operation: "mean",
+    value: findMean(numString),
+  };
+
+  return res.send(result);
+});
+
+// Route to calculate median
+app.get("/median", (req, res) => {
+  // Throw error if empty query strings
+  if (!req.query.nums) throw Error("Missing Query Inputs", 400);
+
+  let numString = req.query.nums.split(",");
+  let result = {
+    operation: "median",
+    value: findMedian(numString),
+  };
+
+  return res.send(result);
+});
+
+// Route to calculate mode
+app.get("/mode", (req, res) => {
+  // Throw error if empty query strings
+  if (!req.query.nums) throw Error("Missing Query Inputs", 400);
+
+  let numString = req.query.nums.split(",");
+  let result = {
+    operation: "mode",
+    value: findMode(numString),
+  };
+
+  return res.send(result);
+});
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
